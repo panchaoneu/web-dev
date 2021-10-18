fedc3538ac9be3aca0a4d35e480bda43f72f19cf
albertchongus <chongx@oregonstate.edu>
2021-10-17 20:48:05 -0700

    a4 css fa21

---
+++ public/a5/practice/todos/todos.js
@@ -0,0 +1,12 @@
+
+
+export default [
+    {   title: 'Buy milk',
+        status: 'CANCELED',
+    },
+    {   title: 'Pickup the kids',
+        status: 'IN PROGRESS',
+    },
+    {   title: 'Walk the dog',
+        status: 'DEFERRED',
+    }, ];
